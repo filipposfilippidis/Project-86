202e54350146ff69466dba675351081112668549
filipposfilippidis <162036413+filipposfilippidis@users.noreply.github.com>
2024-06-25 18:43:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+    fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
+        block_image_object
+    })
+}
+
+function playSound(){
+	
+}
